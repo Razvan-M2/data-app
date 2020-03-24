6f5b1a113eed1a2c10760388ae3431cac6e7e76b
Razvan <razvanmarc180@yahoo.com>
2020-03-24 22:26:24 +0200

    Added a button

---
+++ scripts/behaviour.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+
+    $('#myButton').click(function(){
+
+        var header = document.getElementsByTagName('h1')[0];
+        header.innerHTML = "De ce ai apasat acel buton!?";
+        console.log('here');
+    });
+
+
+
+
+});
