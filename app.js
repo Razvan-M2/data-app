4843f60ea70f910423b05bad5cebf935d6ba65c5
Razvan <razvanmarc180@yahoo.com>
2020-04-12 21:33:02 +0300

    yup

--- app.js
+++ app.js
@@ -8,14 +8,10 @@ require('dotenv').config();
 
 //inputed the object resulted from reading the geoCodes json in a constant
 const countries = JSON.parse(fs.readFileSync(process.env.GEOCODES));
-console.log(countries);
-
-
 
 app.use('/bootstrap-4.4.1-dist',express.static('bootstrap-4.4.1-dist'));
 app.use('/scripts',express.static('scripts'));
 app.use('/style',express.static('style'));
-
 
 app.get('/', (req,res) => {
     res.sendFile(__dirname + '/main.html');
@@ -24,20 +20,21 @@ app.get('/', (req,res) => {
 
 //Get request with plain pathname main2
 app.get('/main2', (req,res) => {
-    //console.log(req.path);
     res.sendFile(__dirname + "/main2.html");
 });
 
 //Middleware example to path main2
 app.use('/main2/:id', (req,res)=>{
     res.sendFile(__dirname + "/main2.html");
-    //dataGoogle.getTrends({keyword: 'Coronavirus',startTime: new Date(Date.now() - (24 * 5 * 60 * 60 * 1000)),geo:'RO'},res);
     console.log(req.params);
 });
 
 //Middleware for trendings by country name translated to geoName in geoCodes
 app.use('/trends/:id', (req,res) => {
-    dataGoogle.getTrends({keyword: 'Coronavirus',startTime: new Date(Date.now() - (24 * 5 * 60 * 60 * 1000)),geo:countries[req.params.id]},res);
+    dataGoogle.getTrends({keyword: 'Coronavirus',
+                          startTime: new Date(Date.now() - (24 * 7 * 60 * 60 * 1000)),
+                          geo:countries[req.params.id]
+                         },res);
 });
 
 module.exports = app;
--- handle_backend/handle_data/googleAPI.js
+++ handle_backend/handle_data/googleAPI.js
@@ -1,7 +1,7 @@
 const googleTrends = require('google-trends-api');
 const beautify = require('json-beautify');
 
-//{keyword: 'Women\'s march'}
+
 module.exports = {
     getTrends : function getTrends(object,res){
 
@@ -9,8 +9,7 @@ module.exports = {
             if(err) console.error('there was an error!', err);
             else{ 
                 var obj = JSON.parse(results);
-                //console.log(beautify(obj.default.geoMapData,null,2,100));
-                //console.log(obj);
+                // console.log(obj.default);
                 res.send(beautify(obj.default.geoMapData,null,2,100));
             };
         })
