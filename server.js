9d1bdebf17713a6839de52b1f91a1e80a05d2c34
Razvan <razvanmarc180@yahoo.com>
2020-04-14 20:12:53 +0300

    added colors module

--- server.js
+++ server.js
@@ -9,5 +9,6 @@ const server = http.createServer(app);
 server.listen(port,(err) => {
     if(err)
         console.log(err);
-    console.log('Now listening to port '+port);
+    console.log('\x1b[4m%s\x1b[36m%s\x1b[0m', 'I am cyan','i am blue');
+    console.log("%cNow listening to port ","color:red"+port);
 });
