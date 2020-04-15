ab535aa1190083d2a982b10dc2cb1bb39cbce12a
Razvan <razvanmarc180@yahoo.com>
2020-04-15 12:19:42 +0300

    added nothing

--- server.js
+++ server.js
@@ -9,6 +9,5 @@ const server = http.createServer(app);
 server.listen(port,(err) => {
     if(err)
         console.log(err);
-    console.log('\x1b[4m%s\x1b[36m%s\x1b[0m', 'I am cyan','i am blue');
     console.log("%cNow listening to port ","color:red"+port);
 });
