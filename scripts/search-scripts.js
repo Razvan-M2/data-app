331efb73456d7a6932da82090b2e01b2e1153945
Razvan <razvanmarc180@yahoo.com>
2020-05-06 22:40:50 +0300

    did some code cleaning

--- scripts/search-scripts.js
+++ scripts/search-scripts.js
@@ -38,17 +38,15 @@ function addData() {
         dataPoints.data.pop();
     } 
 
-
     console.log(arguments);
     for (var i = 0; i < arguments[0].length; i++) {
         if(arguments[0][i].value[0] == 0 )
             continue;
-        // dataPoints.push({
-        //     label: arguments[0][i].geoName,
-        //     y: arguments[0][i].value[0],});
+
         dataPoints.label.push(arguments[0][i].geoName);
         dataPoints.data.push(arguments[0][i].value[0]);
     }
+
     $('#chartContainer').remove();
     $('body').append("<canvas id='chartContainer' style='height: 300px; width: 50%;'></canvas>");
 
@@ -64,7 +62,6 @@ function getData(country){
         type:"get",
         dataType: "json",
         success: function(data){
-            //console.log(data);
             addData(data);
         }
     });
