60e975b3326563a153d1e8272132a9b7299c3c19
Razvan <razvanmarc180@yahoo.com>
2020-05-12 15:52:39 +0300

    arranged the search page

--- app.js
+++ app.js
@@ -3,23 +3,33 @@ const app = express();
 const googleAPI = require('./handle_backend/utils-api/googleAPI');
 const utilityAPI = require('./handle_backend/utilityAPI'); 
 const fs = require('fs');
+const exphbs = require('express-handlebars');
 
 //loaded environment variables from .env file
 require('dotenv').config();
 
+app.use('/font-awesome',express.static('font-awesome'));
 app.use('/bootstrap-4.4.1-dist',express.static('bootstrap-4.4.1-dist'));
 app.use('/scripts',express.static('scripts'));
 app.use('/style',express.static('style'));
 app.use('/charts_js',express.static('charts_js'));
 
-app.get('/', (req,res) => {
-    res.sendFile(__dirname + '/home.html');
+app.engine('hbs', exphbs({
+    extname: '.hbs'
+}));
 
+app.set('view engine', 'hbs');
+ 
+
+app.get('/', (req,res) => {
+    //res.sendFile(__dirname + '/home.html');
+    res.render('home');
 });
 
 //Get request with plain pathname search
 app.get('/search', (req,res) => {
-    res.sendFile(__dirname + "/search.html");
+    //res.sendFile(__dirname + "/search.html");
+    res.render('search');
 });
 
 //Middleware for trendings by country name translated to geoName in geoCodes
@@ -27,7 +37,7 @@ app.use('/trends/:country/:keyword', (req,res) => {
     var countryCode = utilityAPI.countriesAPI.getCountryCodeByName(req.params.country);
     var keyword = req.params.keyword;
     utilityAPI.googleTrendsAPI.getTrends(  {keyword: keyword,
-                                            startTime: new Date(Date.now() - (1 * 24 * 60 * 60 * 1000)),
+                                            startTime: new Date(Date.now() - (7 * 24 * 60 * 60 * 1000)),
                                             geo:countryCode},
                                             res);
 });
